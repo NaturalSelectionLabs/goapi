d0de46ff17acf4517b73d23e6dc5cba77f99f2d8
Yad Smood <y.s.inside@gmail.com>
2023-09-23 12:36:05 +0800

    simplify openapi middleware

--- lib/middlewares/apidoc/apidoc.go
+++ lib/middlewares/apidoc/apidoc.go
@@ -3,6 +3,7 @@ package apidoc
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 	"strings"
 
@@ -23,31 +24,21 @@ func Install(g *goapi.Group, config func(doc *openapi.Document) *openapi.Documen
 
 	op := &operation{}
 
-	g.GET("/", op)
+	g.GET("/openapi.json", op)
 
 	op.doc = config(g.OpenAPI())
 
-	fs := http.FileServer(http.FS(swaggerFiles))
+	dir, _ := fs.Sub(swaggerFiles, "swagger-ui")
 
-	g.Group("/swagger-ui").Use(middlewares.Func(func(next http.Handler) http.Handler {
+	fs := http.FileServer(http.FS(dir))
+
+	g.Use(middlewares.Func(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			r.URL.Path = strings.TrimPrefix(r.URL.Path, g.Prefix())
 			fs.ServeHTTP(w, r)
 		})
 	}))
 }
-
-type params struct {
-	goapi.InHeader
-
-	Accept string `default:"application/json"`
-}
-
-type res interface {
-	goapi.Response
-}
-
-var _ = goapi.Interface(new(res), resOK{}, resRedirect{})
 
 type resOK struct {
 	goapi.StatusOK
@@ -58,42 +49,21 @@ func (resOK) Description() string {
 	return "It will return the OpenAPI doc in JSON format."
 }
 
-type resRedirect struct {
-	goapi.StatusFound
-	Header headerRedirect
-}
-
-func (resRedirect) Description() string {
-	return "It will redirect the browser to the Swagger UI to render the generated OpenAPI doc."
-}
-
-type headerRedirect struct {
-	Location string `description:"Redirect to ./swagger-ui"`
-}
-
 // operation is the operation to serve the OpenAPI document.
 type operation struct {
 	doc *openapi.Document
 }
 
 var _ goapi.OperationOpenAPI = &operation{}
+var _ goapi.OperationHandler = &operation{}
 
 // OpenAPI implements the [goapi.OperationOpenAPI] interface.
 func (*operation) OpenAPI(doc openapi.Operation) openapi.Operation {
-	doc.Description = "It will auto redirect the browser to the Swagger UI to render the generated OpenAPI doc. " +
-		"If you request it with `Accept: application/json` header, it will return the OpenAPI doc in JSON format."
+	doc.Description = "It responds the OpenAPI doc for this service in JSON format."
 	return doc
 }
 
 // Handle implements the [goapi.OperationHandler] interface.
-func (op *operation) Handle(p params, _ *http.Request) res {
-	if strings.Contains(p.Accept, "application/json") {
-		return resOK{Data: op.doc}
-	}
-
-	return resRedirect{
-		Header: headerRedirect{
-			Location: "swagger-ui",
-		},
-	}
+func (op *operation) Handle() resOK {
+	return resOK{Data: op.doc}
 }
